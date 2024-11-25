2713b39a4d29bb92d44c4f6ac96406646fb43475
Adi Kishony <adi.kishony@mail.huji.ac.il>
2024-11-25 13:02:59 +0200

    WIP

--- StimulusMemory/StimulusMemory.py
+++ StimulusMemory/StimulusMemory.py
@@ -1,8 +1,15 @@
+from config_files.closed_loop_config import start_angle, end_angle
+from constants import *
+
+# moving / part1 / part2 / spacer
+MOVING = "moving"
+PART1 = "part1"
+PART2 = "part2"
+SPACER = "spacer"
 
 class StimulusMemory:
     def __init__(self, stimulus_struct):
         """
-        Translates the bout distance and angle to stimuli size angle in the range of 15-165 degrees
         """
         self.startX = int(stimulus_struct["startX"])
         self.startY = int(stimulus_struct["startY"])
@@ -11,11 +18,17 @@ class StimulusMemory:
         self.startShapeRadius = int(stimulus_struct["startShapeRadius"])
         self.endShapeRadius = int(stimulus_struct["endShapeRadius"])
         self.delay = int(stimulus_struct["delay"])
-        self.rep = 1
         self.repetitions = int(stimulus_struct["repetitions"])
-        self.current_angle = int(stimulus_struct["startX"])
-        self.current_size = int(stimulus_struct["startShapeRadius"])
-        self.state = "inactive"
+        self.useSpacer = int(stimulus_struct["useSpacer"])
+
+        self.rep = 0
+        if self.repetitions == -1:
+            self.repetitions = float('inf')
+
+        self.current_angle = self.startX
+        self.current_size = self.startShapeRadius
+        self.state = "inactive" # moving / part1 / part2 / spacer
+        self.stimulus_obj = None
         self.time_ms = 0
         self.max_size = 18
 
@@ -31,7 +44,7 @@ class StimulusMemory:
             distance = 3
         if distance <= 0:
             distance = 0
-        self.current_size = self.current_angle + 2 * distance
+        self.current_size = self.current_size + 2 * distance
         if self.current_size  > self.max_size:
             return "size" # end of trial
         return round(self.current_angle), round(self.current_size)
@@ -40,6 +53,44 @@ class StimulusMemory:
     def reset_loc(self, angle, size):
         self.current_angle = angle
         self.current_size = size
+
+    def update(self):
+        self.time_ms += 1
+        if self.stimulus_obj: # is this stimulus initiated
+            if self.stimulus_obj.status == RUNNING:
+                self.stimulus_obj.move()
+            else:
+                self.rep += 1
+                if self.rep <= self.repetitions:
+                    # either go to spacer or to symmetric stimuli
+                    if self.useSpacer:
+                        pass # init spacer
+                    else:
+                        pass # init symmetric struct
+        else:
+            # see if we need to initiate this stimulus
+            if self.delay <= self.time_ms:
+                pass # init stim struct
+
+    def _init_spacer(self):
+        pass
+
+    def _init_symmetric_stimulus(self):
+        pass
+
+    def _init_stimulus(self):
+        pass
+
+
+
+
+
+
+
+
+    def move(self, moving_angle, moving_dist):
+
+
 
 
 
